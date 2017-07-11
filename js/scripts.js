40c4036fed268f5e2ebb3e4ef64157d9860e8d90
Kaili Nishihira and Lois Choi <kaili@>
2017-07-11 09:35:02 -0700

    Add JS for statement and debug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+  var countToInput = parseInt($("input#countTo").val());
+  var countByInput = parseInt($("input#countBy").val());
 
+    for (var index = 0; index <= countToInput; index += countByInput) {
+    $("ul.results").append("<li>" + index + "</li>");
+    }
 
+  event.preventDefault();
+  });
 });
+
+
+
+
+
+// var inputNumbers = ["countTo", "countBy"];
+//
+// inputNumbers.forEach(function(inputNumber) {
+//   var newNumbers = $("input#" + inputNumbers).val();
